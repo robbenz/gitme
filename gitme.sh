6d7e2d8a6e37fa1e643e3d1eb32bdfd8be3277b3
Rob Benz <benz_rob@yahoo.com>
2015-12-18 09:41:58 -0500

    added an option for a unique or generic commit message

--- gitme.sh
+++ gitme.sh
@@ -9,31 +9,34 @@ REPOS=(
 
 MOVE="Moving to next REPO... \n" 
 
-tput setaf 3;echo What ya wanna do? You can say "push" "pull" "commit" "ftp push" or "status"; tput sgr0
+tput setaf 2;echo "What ya wanna do? You can say push, pull, commit, ftp push, or status"; tput sgr0
 
 read input
 
 if [ $input =  "commit" ]
 then
-   tput setaf 1;echo "Do you want a unique commit message? [y/n]";tput sgr0 ans
-   if [ $ans = "y" ] 
-   then 
-     for i in "${REPOS[@]}"
-    do
-   git add . -A
-   read -p "Commit description: " desc  
-   git commit -m "$desc"
-   done 
-       else 
-           for i in "${REPOS[@]}"
-    do
-        cd "$i"
-        tput setaf 6;pwd;tput sgr0 
-        git add . -A
-        git commit -m "autocommit backup point"
-        tput setaf 2;echo  $MOVE;tput sgr0 
-        sleep 1
-    done 
+    tput setaf 2;echo "Do you want a unique commit message? [y/n]";tput sgr0
+    read ans
+    if [ $ans = "y" ]
+    then 
+        for i in "${REPOS[@]}"
+        do
+            cd "$i"
+            tput setaf 6;pwd;tput sgr0 
+            git add . -A
+            read -p "Commit description: " desc  
+            git commit -m "$desc"
+        done 
+    else 
+        for i in "${REPOS[@]}"
+        do
+            cd "$i"
+            tput setaf 6;pwd;tput sgr0 
+            git add . -A
+            git commit -m "autocommit backup point"
+            tput setaf 2;echo  $MOVE;tput sgr0 
+            sleep 1
+        done
     fi 
 elif [ $input = "push" ] || [ $input = "pull" ] || [ $input = "ftp push" ] || [ $input = "status" ]
     then
